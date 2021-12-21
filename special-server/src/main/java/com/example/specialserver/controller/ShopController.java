a8d14d64ae48d6eb091ef06de12363a6eb60d249
chenqiang <chenqiang@pconline.com.cn>
2021-12-21 12:10:04 +0800

    redis分布式锁优化

--- special-server/src/main/java/com/example/specialserver/controller/ShopController.java
+++ special-server/src/main/java/com/example/specialserver/controller/ShopController.java
@@ -32,73 +32,135 @@ public class ShopController {
         return "成功！";
     }
 
+    @PostMapping("shoping/first")
+    public void  shopingfirst(){
+        try {
+                Integer count = (Integer) redisTemplate.opsForValue().get(keys);
+                if (count > 0){
+                    redisTemplate.opsForValue().set(keys,count-1);
+                    System.out.println("成功抢到商品，库存还剩下："+ (count-1));
+                }else {
+                    System.out.println("商品已经抢购完!!!!!!!!!!!");
+                }
+            System.out.println("交易火爆中，请稍后重试！！！");
+        }catch (Exception e){
+            System.out.println("服务异常!!!!!!!!!!!");
+        }
+    }
 
-    @PostMapping("shoping")
-    public String  shoping(){
+    @PostMapping("shoping/first1")
+    public void  shopingfirst1(){
+        try {
+        synchronized (this){
+            Integer count = (Integer) redisTemplate.opsForValue().get(keys);
+            if (count > 0){
+                redisTemplate.opsForValue().set(keys,count-1);
+                System.out.println("成功抢到商品，库存还剩下："+ (count-1));
+            }else {
+                System.out.println("商品已经抢购完!!!!!!!!!!!");
+            }
+            System.out.println("交易火爆中，请稍后重试！！！");
+        }
+        }catch (Exception e){
+            System.out.println("服务异常!!!!!!!!!!!");
+        }
+    }
 
+
+    @PostMapping("shoping/second")
+    public void  shopingsecond(){
         String lockkey = "lockkey";
         String uuidString = UUID.randomUUID().toString();
-        Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent(lockkey, uuidString, 10, TimeUnit.MILLISECONDS);
-
+        Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent(lockkey, uuidString, 500, TimeUnit.MILLISECONDS);
         try {
-
             if (aBoolean){
                 Integer count = (Integer) redisTemplate.opsForValue().get(keys);
                 if (count > 0){
                     redisTemplate.opsForValue().set(keys,count-1);
                     System.out.println("成功抢到商品，库存还剩下："+ (count-1));
-                    return "成功抢到商品，库存还剩下："+ (count-1);
                 }else {
                     System.out.println("商品已经抢购完!!!!!!!!!!!");
-                    return "商品已经抢购完!!!!!!";
                 }
             }
             System.out.println("交易火爆中，请稍后重试！！！");
-            return "交易火爆中，请稍后重试！！！";
         }catch (Exception e){
             System.out.println("服务异常!!!!!!!!!!!");
-            return "服务异常!!!!!!!!!!!";
+        }
+    }
+
+
+    @PostMapping("shoping/three")
+    public void  shopingThree(){
+        String lockkey = "lockkey";
+        String uuidString = UUID.randomUUID().toString();
+        Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent(lockkey, uuidString, 10, TimeUnit.MILLISECONDS);
+        try {
+            if (aBoolean){
+                Integer count = (Integer) redisTemplate.opsForValue().get(keys);
+                Thread.sleep(10);
+                if (count > 0){
+                    redisTemplate.opsForValue().set(keys,count-1);
+                    System.out.println("成功抢到商品，库存还剩下："+ (count-1));
+                }else {
+                    System.out.println("商品已经抢购完!!!!!!!!!!!");
+                }
+            }
+            System.out.println("交易火爆中，请稍后重试！！！");
+        }catch (Exception e){
+            System.out.println("服务异常!!!!!!!!!!!");
+        }finally {
+                redisTemplate.delete(lockkey);
+        }
+    }
+
+    @PostMapping("shoping/four")
+    public void  shopingFour(){
+        String lockkey = "lockkey";
+        String uuidString = UUID.randomUUID().toString();
+        Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent(lockkey, uuidString, 10, TimeUnit.MILLISECONDS);
+        try {
+            if (aBoolean){
+                Integer count = (Integer) redisTemplate.opsForValue().get(keys);
+                Thread.sleep(5);
+                if (count > 0){
+                    redisTemplate.opsForValue().set(keys,count-1);
+                    System.out.println("成功抢到商品，库存还剩下："+ (count-1));
+                }else {
+                    System.out.println("商品已经抢购完!!!!!!!!!!!");
+                }
+            }
+            System.out.println("交易火爆中，请稍后重试！！！");
+        }catch (Exception e){
+            System.out.println("服务异常!!!!!!!!!!!");
         }finally {
             if (uuidString.equals(redisTemplate.opsForValue().get(lockkey))){
                 redisTemplate.delete(lockkey);
             }
         }
-
     }
 
-    @PostMapping("shoping1")
-    public String  shoping1(){
+    @PostMapping("shoping/five")
+    public void   shopingFive(){
         String lockkey = "lockkey";
         RLock lock = distributedLocker.lock(lockkey);
         try {
-//             distributedLocker.lock(lockkey, TimeUnit.MILLISECONDS, 10);
             lock.lock(10,TimeUnit.MILLISECONDS);
-//            boolean b = lock.tryLock(10, 10, TimeUnit.MILLISECONDS);
-//            if (!b){
-//                System.out.println("活动火爆进行中，请重试....");
-//            }
+            Thread.sleep(5);
             Integer count = (Integer) redisTemplate.opsForValue().get(keys);
             Logger.info("lock:" + lock.toString() + ",interrupted:" + Thread.currentThread().isInterrupted()
                     + ",hold:" + lock.isHeldByCurrentThread() + ",threadId:" + Thread.currentThread().getId());
-
                 if (count > 0){
                     redisTemplate.opsForValue().set(keys,count-1);
                     System.out.println("成功抢到商品，库存还剩下："+ (count-1));
-                    return "成功抢到商品，库存还剩下："+ (count-1);
                 }else {
                     System.out.println("商品已经抢购完!!!!!!!!!!!");
-                    return "商品已经抢购完!!!!!!";
                 }
         }catch (Exception e){
             System.out.println("服务异常!!!!!!!!!!!");
-            return "服务异常!!!!!!!!!!!";
         }finally {
             if (lock.isHeldByCurrentThread()){
                 lock.unlock();
             }
-//            distributedLocker.unlock();
         }
     }
-
-
 }
